e5ad9d599f7a2eda158a265ae5c86abd3bdab389
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-03-14 20:42:44 +0100

    css añadido

---
+++ worker.js
@@ -0,0 +1,18 @@
+//espero el numero
+self.onmessage = function (event) {
+  var n = 1;
+  var primeList = "";
+  search: while(n < event.data) {
+		n += 1;
+		for(var i = 2; i <= Math.sqrt(n); i += 1)
+			if(n % i == 0){
+				continue search;
+      }
+		  primeList += " " + n;
+      if (primeList.length > 499){
+        self.postMessage(primeList);
+        primeList="";
+      }
+	  }
+	self.postMessage(primeList);
+}
